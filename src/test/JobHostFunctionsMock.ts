9a165884b1971657ae313fb5a31feb653bb780b3
Riccardo Balbo <riccardo0blb@gmail.com>
2024-04-19 18:55:29 +0200

    fix mocks

--- src/test/JobHostFunctionsMock.ts
+++ src/test/JobHostFunctionsMock.ts
@@ -5,18 +5,18 @@ export default class JobHostFunctions extends MockFunctions {
     constructor(client: NostrConnectorClient) {
         super("Job");
 
-        this.registerFunction("log", async (mng, jobId, cp, offs: bigint) => {
+        this.registerFunction("log", async (mng, pluginPath, pluginId, jobId, cp, offs: bigint) => {
             const log = cp.read(offs).text();
             // console.log("log ", log);
             this.record("log", { log });
         });
-        this.registerFunction("get", async (mng, currentJobId, cp, offs: bigint) => {
+        this.registerFunction("get", async (mng, pluginPath, pluginId, currentJobId, cp, offs: bigint) => {
             const jobId = cp.read(offs).text() || currentJobId;
             // console.info("get", jobId);
             this.record("get", { jobId });
             return cp.store("{}");
         });
-        this.registerFunction("isDone", async (mng, _, cp, offs: bigint) => {
+        this.registerFunction("isDone", async (mng, pluginPath, pluginId, _, cp, offs: bigint) => {
             const jobId = cp.read(offs).text();
             // console.info("isDone", jobId);
             this.record("isDone", { jobId });
@@ -24,7 +24,16 @@ export default class JobHostFunctions extends MockFunctions {
         });
         this.registerFunction(
             "newInputEventRef",
-            async (mng, _, cp, eventIdOff: bigint, markerOff: bigint, sourceOff: bigint) => {
+            async (
+                mng,
+                pluginPath,
+                pluginId,
+                _,
+                cp,
+                eventIdOff: bigint,
+                markerOff: bigint,
+                sourceOff: bigint
+            ) => {
                 const ref = cp.read(eventIdOff).text();
                 const marker = cp.read(markerOff).text();
                 const source = cp.read(sourceOff).text();
@@ -35,7 +44,16 @@ export default class JobHostFunctions extends MockFunctions {
         );
         this.registerFunction(
             "newInputJobRef",
-            async (mng, _, cp, jobIdOff: bigint, markerOff: bigint, sourceOff: bigint) => {
+            async (
+                mng,
+                pluginPath,
+                pluginId,
+                _,
+                cp,
+                jobIdOff: bigint,
+                markerOff: bigint,
+                sourceOff: bigint
+            ) => {
                 const ref = cp.read(jobIdOff).text();
                 const marker = cp.read(markerOff).text();
                 const source = cp.read(sourceOff).text();
@@ -44,34 +62,31 @@ export default class JobHostFunctions extends MockFunctions {
                 return cp.store("{}");
             }
         );
-        this.registerFunction("newInputData", async (mng, _, cp, dataOff: bigint, markerOff: bigint) => {
-            const data = cp.read(dataOff).text();
-            const marker = cp.read(markerOff).text();
-            // console.info("newInputData", data, marker);
-            this.record("newInputData", { data, marker });
-            return cp.store("{}");
-        });
-        this.registerFunction("newParam", async (mng, _, cp, keyOff: bigint, valuesJsonOffset: bigint) => {
-            const key = cp.read(keyOff).text();
-            const values = cp.read(valuesJsonOffset).text();
-            // console.info("newParam", key, values);
-            this.record("newParam", { key, values });
-            return cp.store("{}");
-        });
         this.registerFunction(
-            "request",
-            async (
-                mng,
-                _,
-                cp,
-                reqOff: bigint,
-             
-            ) => {
-                const req = cp.read(reqOff).json();
-                this.record("request", req);
+            "newInputData",
+            async (mng, pluginPath, pluginId, _, cp, dataOff: bigint, markerOff: bigint) => {
+                const data = cp.read(dataOff).text();
+                const marker = cp.read(markerOff).text();
+                // console.info("newInputData", data, marker);
+                this.record("newInputData", { data, marker });
                 return cp.store("{}");
             }
         );
+        this.registerFunction(
+            "newParam",
+            async (mng, pluginPath, pluginId, _, cp, keyOff: bigint, valuesJsonOffset: bigint) => {
+                const key = cp.read(keyOff).text();
+                const values = cp.read(valuesJsonOffset).text();
+                // console.info("newParam", key, values);
+                this.record("newParam", { key, values });
+                return cp.store("{}");
+            }
+        );
+        this.registerFunction("request", async (mng, pluginPath, pluginId, _, cp, reqOff: bigint) => {
+            const req = cp.read(reqOff).json();
+            this.record("request", req);
+            return cp.store("{}");
+        });
 
         this.expected = {
             log: {
--- src/test/NostrHostFunctionsMock.ts
+++ src/test/NostrHostFunctionsMock.ts
@@ -3,32 +3,44 @@ import MockFunctions from "./MockFunctions";
 export default class NostrHostFunctionsMock extends MockFunctions {
     constructor(client: NostrConnectorClient) {
         super("Nostr");
-        this.registerFunction("sendSignedEvent", async (mng, jobId, cp, eventOff: bigint) => {
-            const event = cp.read(eventOff).text();
-            // console.info("sendSignedEvent", event);
-            this.record("sendSignedEvent", { event });
-            return cp.store("{}");
-        });
-        this.registerFunction("subscribeToEvents", async (mng, jobId, cp, filtersJsonOffset: bigint) => {
-            const filters = cp.read(filtersJsonOffset).json();
-            // console.info("subscribeToEvents", filters);
-            this.record("subscribeToEvents", { filters });
-            return cp.store("{}");
-        });
+        this.registerFunction(
+            "sendSignedEvent",
+            async (mng, pluginPath, pluginId, jobId, cp, eventOff: bigint) => {
+                const event = cp.read(eventOff).text();
+                // console.info("sendSignedEvent", event);
+                this.record("sendSignedEvent", { event });
+                return cp.store("{}");
+            }
+        );
+        this.registerFunction(
+            "subscribeToEvents",
+            async (mng, pluginPath, pluginId, jobId, cp, filtersJsonOffset: bigint) => {
+                const filters = cp.read(filtersJsonOffset).json();
+                // console.info("subscribeToEvents", filters);
+                this.record("subscribeToEvents", { filters });
+                return cp.store("{}");
+            }
+        );
 
-        this.registerFunction("unsubscribeFromEvents", async (mng, jobId, cp, subIdOff: bigint) => {
-            const subscriptionId = cp.read(subIdOff).text();
-            // console.info("unsubscribeFromEvents", subscriptionId);
-            this.record("unsubscribeFromEvents", { subscriptionId });
-            return cp.store("{}");
-        });
+        this.registerFunction(
+            "unsubscribeFromEvents",
+            async (mng, pluginPath, pluginId, jobId, cp, subIdOff: bigint) => {
+                const subscriptionId = cp.read(subIdOff).text();
+                // console.info("unsubscribeFromEvents", subscriptionId);
+                this.record("unsubscribeFromEvents", { subscriptionId });
+                return cp.store("{}");
+            }
+        );
 
-        this.registerFunction("getEvents", async (mng, jobId, cp, subIdOff: bigint, limit: bigint) => {
-            const subscriptionId = cp.read(subIdOff).text();
-            // console.info("getEvents", subscriptionId, limit);
-            this.record("getEvents", { subscriptionId, limit });
-            return cp.store("[]");
-        });
+        this.registerFunction(
+            "getEvents",
+            async (mng, pluginPath, pluginId, jobId, cp, subIdOff: bigint, limit: bigint) => {
+                const subscriptionId = cp.read(subIdOff).text();
+                // console.info("getEvents", subscriptionId, limit);
+                this.record("getEvents", { subscriptionId, limit });
+                return cp.store("[]");
+            }
+        );
 
         this.expected = {
             sendSignedEvent: {
