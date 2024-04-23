1aac7a5adc6f3ff888e0ff72d948b2c554cc1f68
Riccardo Balbo <riccardo0blb@gmail.com>
2024-04-23 17:51:21 +0200

    Fixes, implement, long polling, ignore failing jobs

--- src/runtime/JobManager.ts
+++ src/runtime/JobManager.ts
@@ -11,14 +11,18 @@ import { CallContext, CurrentPlugin } from "@extism/extism";
  import {ExtismFunction, HostFunctionsNamespace} from "./HostFunctionsNamespace";
 import Secrets from "./Secrets";
 import Crypto from "crypto";
-
+type FailedJob = {
+    jobId: string;
+    time: number;
+};
 export default class JobManager {
     conn: PoolConnectorClient;
     loopTimeout: NodeJS.Timeout;
     stopNow: boolean = false;
     jobs: ExtismJob[] = [];
     hostNamespaces: Array<HostFunctionsNamespace> = [];
-    
+    failedJobs: FailedJob[] = [];
+
     constructor(conn: PoolConnectorClient) {
         this.conn = conn;
     }
@@ -39,24 +43,33 @@ export default class JobManager {
 
     async _startPendingJobs() {
         try {
+            
             const pendingJobs: PendingJobs = (
                 await this.conn.getPendingJobs({
                     filterByRunOn: "openagents/extism-runtime",
+                    wait: 60000
                 })
             ).response;
             if (pendingJobs.jobs.length > 0) {
                 console.log(pendingJobs.jobs.length, "jobs to start");
+            }else{
+                console.log("No jobs to start");
             }
+            this.failedJobs = this.failedJobs.filter((fj) => Date.now() - fj.time < 1000 * 60 * 5);
             for (const job of pendingJobs.jobs) {
                 try {
+                    if (this.failedJobs.find((fj) => fj.jobId == job.id)) {
+                        // console.log("Skipping failed job", job.id);
+                        continue;
+                    }
                     const inputs: JobInput[] = job.input;
                     const input = inputs[0];
 
                     const inputData = input.data;
-                    const pluginMain: string = job.param.find((param) => param.key == "main")?.value[0] || "";         
+                    const pluginMain: string = job.param.find((param) => param.key == "main")?.value[0] || "";
                     const maxExecutionTime = job.maxExecutionTime;
                     const expiration = Math.min(Date.now() + maxExecutionTime, job.expiration);
-                    const pluginMainSHAHash=Crypto.createHash('sha256').update(pluginMain).digest('hex');
+                    const pluginMainSHAHash = Crypto.createHash("sha256").update(pluginMain).digest("hex");
 
                     const mergedHostFunctions: {
                         [key: string]: ExtismFunction;
@@ -72,26 +85,24 @@ export default class JobManager {
                             mergedHostFunctions[name] = func;
                         }
                     }
-
-                    const plugin = new ExtismJob(
-                        job.id,
-                        pluginMain,
-                        expiration,
-                        mergedHostFunctions
-                    );
+                    console.log("Running plugin", pluginMain);
+                    const plugin = new ExtismJob(job.id, pluginMain, expiration, mergedHostFunctions);
                     const res = (await this.conn.acceptJob({ jobId: job.id })).response;
+                    console.log("Accepted job", res);
+                    // console.log("Initializing plugin", pluginMain, pluginMainSHAHash,inputData);
                     plugin
                         .init()
                         .then(() => {
                             this.jobs.push(plugin);
                             plugin
                                 .run(inputData)
-                                .then((output) =>
+                                .then((output) => {
+                                    // console.log("Plugin execution completed with output", output);
                                     this.conn.completeJob({
                                         jobId: job.id,
                                         output,
-                                    })
-                                )
+                                    });
+                                })
                                 .catch((e) => {
                                     console.error("Error running job", e);
                                     this.conn.cancelJob({
@@ -109,6 +120,10 @@ export default class JobManager {
                         });
                 } catch (e) {
                     console.error("Error starting job", job.id, e);
+                    this.failedJobs.push({
+                        jobId: job.id,
+                        time: Date.now(),
+                    });
                 }
             }
         } catch (e) {
@@ -133,12 +148,24 @@ export default class JobManager {
         }
     }
 
-    async _loop() {
-        await this._startPendingJobs();
-        await this._jobsLooping();
+    async _loop1() {
         if (this.stopNow) return;
+        await this._jobsLooping();
         this.loopTimeout = setTimeout(() => {
-            this._loop();
+            this._loop1();
+        }, 100);
+    }
+
+    async _loop2() {
+        if (this.stopNow) return;
+        await this._startPendingJobs();
+        this.loopTimeout = setTimeout(() => {
+            this._loop2();
         }, 10);
+    }
+    
+    async _loop() {
+        await this._loop1();   
+        await this._loop2();   
     }
 }
